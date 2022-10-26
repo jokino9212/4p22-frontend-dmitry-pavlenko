07e16b6d7aefcb5cb428353a99d8fb58559ea8cb
Dmitry Pavlenko <104529636+jokino9212@users.noreply.github.com>
2022-10-26 18:49:34 +0300

    add homework_9

---
+++ homework_9/calculator.js
@@ -0,0 +1,50 @@
+'use strict';
+const buttonFinish = document.getElementById('button-finish');
+const resultOut = document.getElementById('result-out');
+
+
+buttonFinish.addEventListener('click', (event) => {
+    event.preventDefault();
+    let firstNumber = document.getElementById('first-number').value;
+    let symbol = document.getElementById('symbol').value;
+    let secondNumber = document.getElementById('second-number').value;
+    let result;
+
+
+    if (firstNumber === '') {
+        return console.log('Первое число не указано')
+    };
+
+    if (secondNumber === '') {
+        return console.log('Второе число не указано')
+    };
+
+   
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+        resultOut.innerText = 'Некорректный ввод чисел';
+        return console.log('Некорректный ввод чисел')
+    };
+
+    switch (symbol) {
+        case '': return console.log('Не введён знак'); break;
+        case '+': result = Number(firstNumber) + Number(secondNumber); break;
+        case '-': result = Number(firstNumber) - Number(secondNumber); break;
+        case '*': result = Number(firstNumber) * Number(secondNumber); break;
+        case '/': result = Number(firstNumber) / Number(secondNumber); break;
+        default: return resultOut.innerText = 'Программа не поддерживает такую операцию', console.log('Программа не поддерживает такую операцию'); break;
+    };
+
+    if (isNaN(result)) {
+        resultOut.innerText = 'Операция некорректна';
+        return console.log('Операция некорректна');
+    };
+
+    if (result === Infinity) {
+        resultOut.innerText = 'Операция некорректна';
+        return console.log('Операция некорректна');
+
+    } else {
+        resultOut.innerText = `${result}`;
+        console.log(result);
+    };
+});
