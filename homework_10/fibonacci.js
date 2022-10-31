9f0ebe213ec14c25409fb66cfb32e09b751d9078
Dmitry Pavlenko <104529636+jokino9212@users.noreply.github.com>
2022-10-31 18:50:39 +0300

    fix homework_10

--- homework_10/fibonacci.js
+++ homework_10/fibonacci.js
@@ -1,15 +1,43 @@
 'use strict';
 
-const fibonacci = ((prev = 0, next = 1) => () => {
-    const result = prev + next;
-    prev = next;
-    next = result;
-    return console.log(result);
-})();
+// addition 
 
-fibonacci(); 
-fibonacci(); 
-fibonacci(); 
-fibonacci(); 
-fibonacci(); 
-fibonacci(); 
+// const fibonacci = ((prev = 0, next = 1) => () => {
+//     const result = prev + next;
+//     prev = next;
+//     next = result;
+//     return console.log(result);
+// })();
+
+//         fibonacci();
+//         fibonacci();
+//         fibonacci();
+//         fibonacci();
+//         fibonacci();
+//         fibonacci(); 
+
+
+// main
+
+function makeFibonacciFunction() {
+    let a = 1;
+    let b = 0;
+    return () => {
+        a = a + b;
+        b = a - b;
+        console.log(a);
+    };
+};
+
+
+const fibonacci = makeFibonacciFunction();
+
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+
+
+
