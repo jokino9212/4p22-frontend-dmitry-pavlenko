e59027b7d581839c10eca6f67282301d2cdfa81c
Dmitry Pavlenko <104529636+jokino9212@users.noreply.github.com>
2022-11-02 11:57:32 +0300

    fix homework_10

--- homework_10/fibonacci.js
+++ homework_10/fibonacci.js
@@ -1,28 +1,11 @@
 'use strict';
 
-// addition 
-
-// const fibonacci = ((prev = 0, next = 1) => () => {
-//     const result = prev + next;
-//     prev = next;
-//     next = result;
-//     return console.log(result);
-// })();
-
-//         fibonacci();
-//         fibonacci();
-//         fibonacci();
-//         fibonacci();
-//         fibonacci();
-//         fibonacci(); 
-
-
 // main
 
 function makeFibonacciFunction() {
     let a = 1;
-    let b = 0;
-    return () => {
+    let b = -1;
+    return function()  {
         a = a + b;
         b = a - b;
         console.log(a);
@@ -38,6 +21,48 @@ fibonacci();
 fibonacci();
 fibonacci();
 fibonacci();
+
+
+
+
+// addition *
+
+// const fibonacci = (function makeFibonacciFunction() {
+//     let a = 1;
+//     let b = -1;
+//     return function()  {
+//         a = a + b;
+//         b = a - b;
+//         console.log(a);
+//     };
+// })();
+
+
+// fibonacci();
+// fibonacci();
+// fibonacci();
+// fibonacci();
+// fibonacci();
+// fibonacci();
+
+// addition IIFE arrow function 
+
+// const fibonacci = ((prev = -1, next = 1) => () => {
+//     const result = prev + next;
+//     prev = next;
+//     next = result;
+//     return console.log(result);
+// })();
+
+//         fibonacci();
+//         fibonacci();
+//         fibonacci();
+//         fibonacci();
+//         fibonacci();
+//         fibonacci(); 
+
+
+
 
 
 
